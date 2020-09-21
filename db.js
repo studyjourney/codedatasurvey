a0c4179bf37aae9c9d4ca26728871a19dd17c68b
Leon Rudowicz <leon@rudowicz.de>
2020-09-21 13:28:51 +0100

    Check DB Connection and create sample .env file

---
+++ db.js
@@ -0,0 +1,23 @@
+/**
+ * 
+ * Example MySQL Connection with .env Credentials
+ * 
+ */
+
+const mysql = require('mysql');
+require('dotenv').config();
+
+let connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+});
+
+connection.connect(function(err) {
+    if (err) {
+      return console.error('error: ' + err.message);
+    }
+  
+    console.log('Connected to the MySQL server.');
+  });
