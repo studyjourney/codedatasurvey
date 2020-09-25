7a13680eaca7dee2ba384e516a1b15779035c26d
William Dry <wpbdry@gmail.com>
2020-09-25 13:17:52 +0200

    validate token before adding student in DB

--- index.js
+++ index.js
@@ -30,8 +30,10 @@ app.get('/ping', function (req, res) {
 app.post('/codeData', async (req, res) => {
     try {
         const { token, permissions } = req.body
+        //Query API before creating a new student in the DB in case the token is not valid
+        const assessments = await getMyStudies(token, permissions.projects)
         const studentId = await db.newStudent()
-        await db.writeAssessments(studentId, await getMyStudies(token, permissions.projects))
+        await db.writeAssessments(studentId, assessments)
         if (permissions.currentSemester) await db.writeCurrentSemester(studentId, await getCurrentSemester(token))
         if (permissions.events) await db.writeEvents(studentId, await getEvents(token))
         if (permissions.projects) await db.writeProjects(studentId, await getProjects(token))
