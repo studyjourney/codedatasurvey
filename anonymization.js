58e4245d935db29fa5bacf04f1fe269ca4a95698
Santi <santiago.martinezavial@gmail.com>
2020-10-08 11:45:46 +0200

    renamed anon to js and replaced anonyimization.js

--- anonymization.js
+++ anonymization.js
@@ -1,37 +1,112 @@
-/**
- * THIS IS A WIP COMMIT. PLEASE USE `git commit --amend` WHEN IT'S DONE
- */
+const { connection } = require('./db')
 
-export class NameReplacer {
-    /**
-     * Connects to the database and fetches all names on init().
-     * But init can't be in the construcotr cause constructor has to be sync.
-     * There also needs to be a concept of "run", or something in which the numbering is kept.
-     * Or simpley a reset() method
-     */
-    constructor() {
-        this.allNames = []
-        this.usedNames = []
+async function getNameList() {
+    const getNames = await connection.query('SELECT * FROM code_user_names')
+    var nameList = []
+    for (var i in getNames) {
+        nameList.push(getNames[i].name)
     }
-    async init() {
-        this.allNames = await ['some', 'db', 'function']
+    return nameList
+}
+
+async function getExistingAssessmentNotes() {
+    const getNotes = await connection.query('SELECT external_notes, internal_notes FROM assessments')
+    var notesList = []
+    for (var i in getNotes) {
+        notesList.push({
+            internal: getNotes[i].internal_notes,
+            external: getNotes[i].external_notes
+        })
     }
-    reset() {
-        this.usedNames = []
+    return notesList
+}
+
+
+// maybe other names would be smarter
+// Returns anonymized Assessment Notes
+async function notesAnonymizer(internal_notes, external_notes) {
+    const userNameList = await getNameList()
+    var assessmentNotes = {
+        internal: internal_notes,
+        external: external_notes
     }
-    pseudoNameFromUsedName(usedName) {
-        return `Person${this.usedNames.indexOf(usedName)}`
+    return replaceNamesInNotes(assessmentNotes, userNameList)
+}
+
+// maybe other names would be smarter
+// Retroactively anonymize all non-censored names in the database.
+async function retroactiveNotesAnonymizer() {
+    const userNameList = await getNameList()
+    const assessmentNoteList = await getExistingAssessmentNotes()
+
+    // Every set of assessment notes in db
+    for (var x in assessmentNoteList) {
+        var assessmentNotes = assessmentNoteList[x]
+        assessmentNoteList[x] = replaceNamesInNotes(assessmentNotes, userNameList)
     }
-    replaceWord(word) {
-        if (this.usedNames.includes(word)) return this.pseudoNameFromUsedName(word)
-        if (this.allNames.includes(word)) {
-            this.usedNames.push(word)
-            return this.pseudoNameFromUsedName(word)
+    return assessmentNoteList
+}
+
+function replaceNamesInNotes(assessmentNotes, userNameList) {
+    var usedNames = listNamesInText(assessmentNotes, userNameList);
+    var fixedInternalNotes = assessmentNotes.internal
+    var fixedExternalNotes = assessmentNotes.external
+    for (var i in usedNames) {
+        var replaceWord = `Person${+i + +1}`
+        var regExpName = new RegExp('(' + usedNames[i] + ')', 'gi')
+        // I feel like this can be cleaner
+        if (!assessmentNotes.internal && !assessmentNotes.external) {
+        } else if (!assessmentNotes.external) {
+            fixedInternalNotes = fixedInternalNotes.replace(regExpName, replaceWord)
+        } else if (!assessmentNotes.internal) {
+            fixedExternalNotes = fixedExternalNotes.replace(regExpName, replaceWord)
+        } else {
+            fixedInternalNotes = fixedInternalNotes.replace(regExpName, replaceWord)
+            fixedExternalNotes = fixedExternalNotes.replace(regExpName, replaceWord)
         }
-        return word
     }
-    replaceAllInText(text) {
-        // TODO: Think about commas, \n etc
-        return text.split(' ').map(word => this.replaceWord(word)).join(' ')
+    assessmentNotes.internal = fixedInternalNotes
+    assessmentNotes.external = fixedExternalNotes
+
+    return assessmentNotes
+}
+
+// References list of usernames against the notes and returns a list of all ocurring names
+function listNamesInText(assessmentNotes, userNameList) {
+    var usedNames = [];
+
+    for (var i in userNameList) {
+        if (!assessmentNotes.internal && !assessmentNotes.external) {
+            return []
+        } else if (!assessmentNotes.external) {
+            usedNames.pushIfExists(lookForNames(userNameList[i], assessmentNotes.internal))
+        } else if (!assessmentNotes.internal) {
+            usedNames.pushIfExists(lookForNames(userNameList[i], assessmentNotes.external))
+        } else {
+            usedNames.pushIfExists(lookForNames(userNameList[i], assessmentNotes.internal))
+            usedNames.pushIfExists(lookForNames(userNameList[i], assessmentNotes.external))
+        }
+    }
+    return usedNames
+}
+
+function lookForNames(userName, notes) {
+    if (notes.toLowerCase().replace(/\n/g, " ").split(" ").includes(userName)) {
+        return userName
     }
 }
+
+
+// Custom push method that only pushes if the passed item is not null or false
+Array.prototype.pushIfExists = function (element) {
+    if (element) {
+        this.push(element)
+        return true;
+    }
+    return false;
+}
+
+module.exports = {
+    notesAnonymizer,
+    retroactiveNotesAnonymizer
+};
